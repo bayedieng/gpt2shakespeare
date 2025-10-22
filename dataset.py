599b10e72c8a1c83fbd07acce04009f75eaf39e2
Baye Dieng <bayedieng98@gmail.com>
2025-10-22 23:03:47 +0000

    add dataset

---
+++ dataset.py
@@ -0,0 +1,41 @@
+from torch.utils.data import Dataset
+import torch
+from tokenizers import Tokenizer
+import prep_data
+
+class Gpt2ShakespeareDatset(Dataset):
+    def __init__(self):
+        self.tokenizer: Tokenizer = Tokenizer.from_file("tokenizer.json")
+        self.data = prep_data.load_data()
+        self.tokens = []
+        self.token_ids = []
+        self.data_tokens = [self.tokenizer.encode(x) for x in self.data]
+
+        for i in range(len(self.data_tokens)):
+            tokens = self.data_tokens[i].tokens
+            ids = self.data_tokens[i].ids
+
+            # Append BOS and EOS tokens at the start/end of every play
+            tokens.insert(0, "<|bos|>")
+            ids.insert(0, 1)
+            tokens.append("<|eos|>")
+            ids.append(2)
+            self.tokens.append(tokens)
+            self.token_ids.append(ids)
+
+        # Split tensors into batches of 512 tokens
+        tensors = torch.tensor([id_entries for id in self.token_ids for id_entries in id])
+        window_size = 512 + 1
+        B = tensors.numel() // window_size
+        trimmed_tensors = tensors[:B * window_size]
+        trimmed_tensors = trimmed_tensors.view(B, window_size)
+        self.x = trimmed_tensors
+        self.y = trimmed_tensors[:, 1:]
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, index):
+        return self.x[index], self.y[index]
+
+dataset = Gpt2ShakespeareDatset()
---
+++ tokenizer.py
@@ -0,0 +1,25 @@
+from tokenizers import Tokenizer
+from tokenizers.models import BPE
+from tokenizers.pre_tokenizers import ByteLevel
+from tokenizers.trainers import BpeTrainer
+from tokenizers.decoders import ByteLevel as ByteLevelDecoder
+import prep_data
+
+
+if __name__ == "__main__":
+    tok = Tokenizer(BPE(unk_token="<|unk|>"))
+    tok.pre_tokenizer = ByteLevel(add_prefix_space=True)
+    tok.decoder = ByteLevelDecoder()
+
+    trainer = BpeTrainer(
+        vocab_size=8000,
+        special_tokens=["<|unk|>", "<|bos|>", "<|eos|>"],
+    )
+    prepped_data = prep_data.load_data()
+
+    def lines():
+        for x in prepped_data:
+            yield x
+
+    tok.train_from_iterator(lines(), trainer=trainer)
+    tok.save("tokenizer.json")
