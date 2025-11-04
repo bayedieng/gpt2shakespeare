d721227ad74195b8185c279bef6be2017f2f96a0
Baye Dieng <bayedieng98@gmail.com>
2025-11-04 12:55:11 +0000

    add ffn and positional encoding

--- model.py
+++ model.py
@@ -5,6 +5,23 @@ import math
 
 N_HEADS = 12
 D_MODEL = 768
+D_FF = D_MODEL * 4
+N_LAYERS = 4
+
+
+class PosEncoding(nn.Module):
+    def __init__(self):
+        super().__init__()
+        position = torch.arange(5000)  # 5000 from the torch docs
+        div_term = torch.exp(torch.arange(0, D_MODEL))
+        pe = torch.zeros(5000, 1, D_MODEL)
+        pe[:, 0, 0::2] = torch.sin(position * div_term)
+        pe[:, 0, 1::2] = torch.cos(position * div_term)
+        self.register_buffer("pe", pe)
+
+    def forward(self, x):
+        x = x + self.pe[: x.size(1)]
+
 
 class Attention(nn.Module):
     def __init__(self):
@@ -19,10 +36,10 @@ class Attention(nn.Module):
         self.linear_w_out = nn.Linear(self.d_model, self.d_model)
 
     def split(self, x):
-        '''
+        """
         input -> [batch_size, seq_len, d_model]
         return -> [batch_size, head, seq_len, d_k]
-        '''
+        """
         batch_size, seq_len, d_model = x.size()
         x = x.view(batch_size, self.n_heads, seq_len, self.d_k)
         return x
@@ -30,16 +47,15 @@ class Attention(nn.Module):
     def attention(self, q, k, v):
         k = k.transpose(2, 3)
         qk_mul = torch.matmul(q, k)
-        attn_out = F.softmax(torch.matmul(qk_mul / math.sqrt(self.d_k), v), dim=1)
+        attn_out = F.softmax(torch.matmul(qk_mul / math.sqrt(self.d_k), v), dim=-1)
         return attn_out
-
 
     def forward(self, x):
         # [batch_size, seq_len, d_model]
         q, k, v = self.linear_q(x), self.linear_k(x), self.linear_v(x)
 
         # split into heads
-        q,k,v = self.split(q), self.split(k), self.split(v)
+        q, k, v = self.split(q), self.split(k), self.split(v)
 
         # apply attention and concat output
         attn_out = self.attention(q, k, v)
@@ -47,9 +63,22 @@ class Attention(nn.Module):
         # concat out as multi-head
         batch_size, n_heads, seq_len, d_k = attn_out.size()
         d_model = n_heads * d_k
-        multi_head = attn_out.view(batch_size,seq_len, d_model)
+        multi_head = attn_out.view(batch_size, seq_len, d_model)
 
         # project into the two final layers
         multi_head = self.linear_w_out(multi_head)
         out = self.linear_final_layer(multi_head)
         return out
+
+
+class FFN(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.l1 = nn.Linear(D_MODEL, D_FF)
+        self.l2 = nn.Linear(D_FF, D_MODEL)
+
+    def forward(self, x):
+        x = self.l1(x)
+        x = F.relu(x)
+        x = self.l2(x)
+        return x
